9d42f8dafe7942a7aa5d89ced70b7ac84ad3aa1e
= <distancemezhin@gmail.com>
2022-12-29 04:34:41 +0500

    Added docstrings.

--- main.py
+++ main.py
@@ -11,47 +11,84 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def as_text(value):
-    if value is None:
-        return ""
-    return str(value)
-
-
 class Vacancy:
+    """Класс для представления вакансии.
+
+    Attributes:
+        name (str): Имя вакансии
+        salary_from (int): Нижняя граница вилки оклада
+        salary_to (int): Верхняя граница вилки оклада
+        salary_currency (str): Валюта оклада
+        salary_average (float): Среднее значение оклада в рублях
+        area_name (str): Город, в котором представлена вакансия
+    """
     currency_to_rub = {
         "AZN": 35.68, "BYR": 23.91, "EUR": 59.90, "GEL": 21.74, "KGS": 0.76,
-        "KZT": 0.13, "RUR": 1, "UAH": 1.64, "USD": 60.66, "UZS": 0.0055,
-    }
+        "KZT": 0.13, "RUR": 1, "UAH": 1.64, "USD": 60.66, "UZS": 0.0055, }
 
     def __init__(self, vacancy):
+        """Инициализирует объект Vacancy
+
+        Args:
+            vacancy (dict): Словарь с информацией о вакансии
+        """
         self.name = vacancy['name']
-        self.salary_from = int(float(vacancy['salary_from']))
         self.salary_to = int(float(vacancy['salary_to']))
         self.salary_currency = vacancy['salary_currency']
+        self.salary_from = int(float(vacancy['salary_from']))
         self.salary_average = self.currency_to_rub[self.salary_currency] * (self.salary_from + self.salary_to) / 2
         self.area_name = vacancy['area_name']
         self.year = int(vacancy['published_at'][:4])
 
+
 class DataSet:
+    """Класс для парсинга csv и сбора необходимой информации о вакансиях.
+
+        Attributes:
+            filename (str): Имя csv файла
+            vacancy_name (str): Имя выбранной вакансии
+        """
     def __init__(self, filename, vacancy_name):
         self.filename = filename
         self.vacancy_name = vacancy_name
 
     @staticmethod
-    def add(dict, key, number):
+    def add(dict, key, value):
+        """Добавляет выбранное значение к значению элемента по ключу или присваивает это значение, если по данному ключу не найдено элементов.
+
+            Args:
+                dict (dict): Словарь, по отношению к которому применяем операцию
+                key (str): Ключ словаря, к значению которого применяем операцию
+                value (str or float or int or double): Значение, которое необходимо добавить к сущестующему значению элемента по ключу или присвоить, если значения по ключу не существует
+        """
         if key in dict:
-            dict[key] += number
+            dict[key] += value
         else:
-            dict[key] = number
+            dict[key] = value
 
     @staticmethod
-    def avg(dictionary):
+    def avg(dict):
+        """ Получить словарь, в котором все ключи такие же, как и в переданном, а значения - средние значения всех элементов по данному ключу из основного словаря.
+
+            Args:
+                dict (dict): Словарь, на основе которого будет возвращён новый
+
+            Returns:
+                dict: Новый словарь
+
+        """
         new_dict = {}
-        for key, values in dictionary.items():
+        for key, values in dict.items():
             new_dict[key] = int(sum(values) / len(values))
         return new_dict
 
     def read_csv(self):
+        """Чтение csv и формирование словарей с информацией о вакансиях.
+
+            Returns:
+                dict: Ленивый возврат словарей с информацией о вакансиях
+        """
+
         with open(self.filename, mode='r', encoding='utf-8-sig') as file:
             reader = csv.reader(file)
             header = next(reader)
@@ -61,6 +98,12 @@ class DataSet:
                     yield dict(zip(header, row))
 
     def get_info(self):
+        """Получение информации для статистики.
+
+            Returns:
+                (dict,dict,dict,dict,dict,dict): 1.Зарплаты по годам, 2.Количество вакансий по годам, 3.Зарплаты по годам для выбранной профессии,
+                    4.Количество вакансий по годам для выбранной профессии, 5.Зарплаты по городам, 6.Доля вакансий по городам
+        """
         salary = {}
         salary_of_vacancy_name = {}
         salary_city = {}
@@ -93,9 +136,27 @@ class DataSet:
         area_count = dict(area_count[:10])
         return years_salary, years_count, years_salary_vac, years_count_vac, area_salary, area_count
 
+
 class Report:
-    def generate_excel(self, data1, column_names, column_names2, data2):
+    """Класс для формирования excel, pdf, png и текстового отчётов."""
+    @staticmethod
+    def generate_excel(data1, column_names, column_names2, data2):
+        """Формирование excel отчёта в каталоге проекта.
+
+            Args:
+                data1 (list of dict): Список словарей с информацией для первого листа
+                column_names (list of str): Названия столбцов первого листа
+                column_names2 (list of str): Названия столбцов второго листа
+                data2 (list of dict): Список словарей с информацией для второго листа
+        """
+        @staticmethod
         def auto_width(sheets):
+            """Метод для установки автоматического размера ячейки на выбранных листах Workbook.
+
+                Args:
+                    sheets (list of Workbook): Список листов
+            """
+
             for sheet in sheets:
                 for sheet_column_cells in sheet.columns:
                     new_column_length = max(len(str(cell.value)) for cell in sheet_column_cells)
@@ -146,6 +207,17 @@ class Report:
 
     @staticmethod
     def generate_image(data1, profession_name, data2, data3, data4):
+        """Формирование png отчёта в каталоге проекта.
+
+             Args:
+                 data1 (list of dict): Cписок, включающий в себя: словарь с зарплатами по годам, словарь с зарплатами по годам
+                    для выбранной вакансии
+                 profession_name (str): Выбранная профессия
+                 data2 (list of dict): Cписок, включающий в себя: словарь с количеством вакансий по годам, словарь с
+                    количеством вакансий по годам для выбранной вакансии
+                 data3 (dict): Словарь с уровнями зарплат по городам
+                 data4 (dict): Словарь с долями вакансий по городам
+         """
         fig,axs = plt.subplots(2, 2)
         #Первый график
         width = 0.4
@@ -197,6 +269,17 @@ class Report:
 
     @staticmethod
     def generate_pdf(profession_name, data1, column_names1, data2, column_names2, data3, column_names3):
+        """Формирование png отчёта в каталоге проекта.
+
+             Args:
+                 profession_name (str): Выбранная профессия
+                 data1 (list of dict): Cписок, включающий в себя: словарь с зарплатами по годам, словарь с зарплатами по годам
+                    для выбранной вакансии, словарь с количеством вакансий по годам, словарь с количеством вакансий по годам
+                    для выбранной вакансии
+                 column_names1 (list of str): Названия столбцов первой таблицы
+                 data2 (dict): Словарь с уровнями зарплат по городам
+                 data3 (dict): Словарь с долями зарплат по городам
+        """
         config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
         env = Environment(loader=FileSystemLoader('.'))
         template = env.get_template("pdf_template.html")
@@ -225,7 +308,18 @@ class Report:
                                         'table3_columns':table3_columns, 'table3_rows':table3_rows})
         pdfkit.from_string(pdf_template, 'report.pdf', configuration=config, options={"enable-local-file-access": ""})
 
-    def print_report(self, years_salary, years_count, years_salary_vac, years_count_vac, area_salary, area_count):
+    @staticmethod
+    def print_report(years_salary, years_count, years_salary_vac, years_count_vac, area_salary, area_count):
+        """Формирование и вывод текстового отчёта.
+
+             Args:
+                 years_salary (dict): Словарь с зарплатами по годам
+                 years_count (dict): Словарь с количеством вакансий по годам
+                 years_salary_vac (dict): Словарь с зарплатами по годам для выбранной профессии
+                 years_count_vac (dict): Словарь с количеством вакансий по годам для выбранной профессии
+                 area_salary (dict): Словарь с уровнями зарплат по городам
+                 area_count (dict): Словарь с долями вакансий по городам
+        """
         print("Динамика уровня зарплат по годам: " + str(years_salary))
         print("Динамика количества вакансий по годам: " + str(years_count))
         print("Динамика уровня зарплат по годам для выбранной профессии: " + str(years_salary_vac))
@@ -233,14 +327,43 @@ class Report:
         print("Уровень зарплат по городам (в порядке убывания): " + str(area_salary))
         print("Доля вакансий по городам (в порядке убывания): " + str(area_count))
 
+
 class Program:
+    """Класс для инициализации необходимых для работы программы данных.
+
+        Attributes:
+            program_action (str): Выбор необходимого алгоритма (Статистика - для формирования png, excel и pdf отчётов,
+                Вакансии - для формирования текстового отчёта)
+            report (Report): Экземпляр класса Report
+            dataset (DataSet): Экземпляр класса DataSet
+            years_salary_dic (dict): Словарь с зарплатами по годам
+            years_count_dic (dict): Словарь с количеством вакансий по годам
+            years_salary_vac_dic (dict): Словарь с зарплатами по годам для выбранной профессии
+            years_count_vac_dic (dict): Словарь с количеством вакансий по годам для выбранной профессии
+            area_salary_dic (dict): Словарь с уровнями зарплат по городам
+            area_count_dic (dict): Словарь с долями вакансий по городам
+
+    """
     def __init__(self):
+        """Инициализирует объект Program, присваивает переменным необходимые данные на основе пользовательского ввода."""
         self.program_action = input('Введите данные для печати: ')
         self.report = Report()
         self.dataset = DataSet(input("Введите название файла: "), input("Введите название профессии: "))
         self.years_salary_dic, self.years_count_dic, self.years_salary_vac_dic, self.years_count_vac_dic, self.area_salary_dic, self.area_count_dic = self.dataset.get_info()
 
+    @staticmethod
+    def as_text(value):
+        """Конвертирует значение какого-либо типа в строку
+
+            Returns:
+                str: Строковое представление значения
+        """
+        if value is None:
+            return ""
+        return str(value)
+
     def run(self):
+        """Запускает необходимые для формирования выбранного типа отчёта операции."""
         if self.program_action == 'Вакансии':
             self.report.print_report(self.years_salary_dic, self.years_count_dic, self.years_salary_vac_dic,
                                      self.years_count_vac_dic, self.area_salary_dic, self.area_count_dic)
@@ -257,8 +380,8 @@ class Program:
             for i in range(0, len(self.area_count_dic)-1):
                 value = str('%.2f' % round(list(self.area_count_dic.values())[i]*100, 2)+'%')
                 table3_data[list(self.area_count_dic.keys())[i]] = value
-            self.report.generate_pdf('Программист',first_table_data,['Год', 'Средняя зарплата', 'Средняя зарплата - Программист', 'Количество вакансий',
-                        'Количество вакансий - Программист'],self.area_salary_dic,['Город', 'Уровень зарплат'],table3_data,['Город', 'Доля вакансий'])
+            self.report.generate_pdf(self.dataset.vacancy_name,first_table_data,['Год', 'Средняя зарплата', 'Средняя зарплата - {0}'.format(self.dataset.vacancy_name), 'Количество вакансий', 'Количество вакансий - {0}'.format(self.dataset.vacancy_name)],
+                                     self.area_salary_dic,['Город', 'Уровень зарплат'], table3_data, ['Город', 'Доля вакансий'])
 
 
 program = Program()
