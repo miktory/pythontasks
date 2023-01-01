47c6498b36200b8b92a7d71b4c4d69fe7266aab0
= <distancemezhin@gmail.com>
2023-01-01 11:00:25 +0500

    3.5.2.py

--- 3.5.2.py
+++ 3.5.2.py
@@ -8,8 +8,7 @@ def edit_salary(date, salary_from, salary_to, salary_currency, cursor):
     currency_exchange = 0
     if salary_currency in ["BYN", "BYR", "EUR", "KZT", "UAH", "USD"]:
         salary_currency.replace("BYN", "BYR")
-        date = f"{date[0]}-{date[1]}"
-        currency_exchange = cursor.execute("select * from currencies where date == :date", {"date": date}).fetchone()[currencies_num[salary_currency]]
+        currency_exchange = cursor.execute("select * from currencies where date == :published", {"published": f"{date[0]}-{date[1]}"}).fetchone()[currencies_num[salary_currency]]
     elif salary_currency == "RUR":
         currency_exchange = 1
     if not (math.isnan(salary_from)) and not (math.isnan(salary_to)):
@@ -26,7 +25,7 @@ def edit_salary(date, salary_from, salary_to, salary_currency, cursor):
 currencies_num = {"BYR": 2, "USD": 6, "EUR": 3, "KZT": 4, "UAH": 5}
 connection = sqlite3.connect("currencies.db")
 pd.set_option("expand_frame_repr", False)
-df = pd.read_csv(input("Введите название файла (vacancies_dif_currencies.csv): "))
+df = pd.read_csv(input("Введите имя файла (vacancies_dif_currencies.csv): "))
 cur = connection.cursor()
 df["salary"] = df.apply(lambda x: edit_salary(x["published_at"][:7].split("-"), x["salary_from"], x["salary_to"], x["salary_currency"], cur), axis=1)
 df["published_at"] = df.apply(lambda x: x["published_at"][0:7], axis=1)
